39f8f1fc3c77eef8fe3e645fc78512426d4d9b6b
James Knighton <iamknighton@gmail.com>
2022-06-24 06:19:40 -0700

    drop_sphere.

--- composer/algorithms/drop_sphere/drop_sphere.py
+++ composer/algorithms/drop_sphere/drop_sphere.py
@@ -1,4 +1,5 @@
 from math import ceil
+import numpy as np
 import torch
 from typing import Optional
 from torch import nn
@@ -9,39 +10,68 @@ from composer.utils import module_surgery
 
 
 class DropSphereNd(nn.Module):
+    """DropSphere module.
 
-    def __init__(self, dim: int, rate: float, embed_dim: int = 16):
+    Args:
+        rate (float): Usage rate in training mode (from 0 to 1). (Default: ``0.1``)
+        power (float): Channels to not drop, as a power of all channels (from 0 to 1). (Default: ``0.9``)
+        embed_dim (int): Dimensionality of channel embeddings. (Default: ``16``)
+    """
+
+    def __init__(self, rate: float = 0.1, power: float = 0.9, embed_dim: int = 16):
         super().__init__()
-        self.dim = dim
         self.rate = rate
+        self.power = power
         self.embed_dim = embed_dim
-        self.register_buffer('embeds', torch.randn(embed_dim, dim))
+        self.embeds = None
+        self.was_training = False
+        self.many_epochs = 100
+        self.epoch = -1
 
     def forward(self, x):
+        if not self.was_training and self.training:
+            self.epoch += 1
+        self.was_training = self.training
+
         if not self.training:
             return x
+
+        progress = np.tanh(1.5 * self.epoch / self.many_epochs) ** 2
+        if self.rate * progress < np.random.uniform():
+            return x
+
+        if self.embeds is None:
+            self.register_buffer('embeds', torch.randn(self.embed_dim, x.shape[1], device=x.device))
+
         n, c = x.shape[:2]
         embeds = torch.randn(n, self.embed_dim, device=x.device)
         activ = torch.einsum('ne,ed->nd', [embeds, self.embeds])
-        power = 1 - self.rate
-        index = ceil(c ** power)
-        mins = activ.sort(1).values[:, index:index + 1]
-        keep = (mins <= activ).type(x.dtype)
+
+        index = ceil(c ** self.power) - 1
+        maxes = activ.sort(1).values[:, index:index + 1]
+        keep = (activ <= maxes).type(x.dtype)
         shape = (n, c) + (1,) * (x.ndim - 2)
         keep = keep.view(shape)
-        return x * keep * c / (c - index)
+
+        return x * keep / keep.mean()
 
 
-def replace(module: nn.Module, index: int):
-    if not index:
-        return module
-    return nn.Sequential(
-        DropSphereNd(module.in_channels, 0.9),
-        module,
-    )
+def apply_drop_sphere(model: nn.Module, rate: float = 0.1, power: float = 0.9, embed_dim: int = 16):
+    """Apply drop sphere to the model.
 
+    Args:
+        rate (float): Usage rate in training mode (from 0 to 1). (Default: ``0.1``)
+        power (float): Channels to not drop, as a power of all channels (from 0 to 1). (Default: ``0.9``)
+        embed_dim (int): Dimensionality of channel embeddings. (Default: ``16``)
+    """
+    def replace(module: nn.Module, index: int):
+        if not index:
+            return module
+        return nn.Sequential(
+            DropSphereNd(rate, power, embed_dim),
+            module,
+        )
 
-def apply_drop_sphere(model: nn.Module):
     transforms = {
         nn.Conv1d: replace,
         nn.Conv2d: replace,
@@ -52,18 +82,21 @@ def apply_drop_sphere(model: nn.Module):
 
 
 class DropSphere(Algorithm):
-    """DropSphere.
+    """DropSphere algorithm.
 
     Args:
-        rate (float): Rate. Default: ``0.9``.
+        rate (float): Usage rate in training mode (from 0 to 1). (Default: ``0.1``)
+        power (float): Channels to not drop, as a power of all channels (from 0 to 1). (Default: ``0.9``)
+        embed_dim (int): Dimensionality of channel embeddings. (Default: ``16``)
     """
 
-    def __init__(self, rate: float = 0.9) -> None:
+    def __init__(self, rate: float = 0.1, power: float = 0.9, embed_dim: int = 16):
         self.rate = rate
+        self.power = power
+        self.embed_dim = embed_dim
 
     def match(self, event: Event, state: State) -> bool:
         return event == Event.INIT
 
     def apply(self, event: Event, state: State, logger: Optional[Logger] = None) -> None:
-        assert state.model is not None, 'Model must be in state'
-        apply_drop_sphere(state.model)
+        apply_drop_sphere(state.model, self.rate, self.power, self.embed_dim)
